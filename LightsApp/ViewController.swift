80c50a859fb406ed0dd8e6ba91379f81e10b97a8
Sergey Ivanov <regspam88@ya.ru>
2022-03-02 15:07:43 +0300

    Setup lights

--- LightsApp/ViewController.swift
+++ LightsApp/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var controlButton: UIButton!
+//    var currentLight = 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redLightView.layer.cornerRadius = 77
+        redLightView.alpha = 0.3
+        
+        yellowLightView.layer.cornerRadius = 77
+        yellowLightView.alpha = 0.3
+        
+        greenLightView.layer.cornerRadius = 77
+        greenLightView.alpha = 0.3
+        
+        controlButton.layer.cornerRadius = 15
+        
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func controlButtonPressed() {
+//        controlButton.setTitle("NEXT", for: .normal)
+       
+        
+        controlButton.setTitle("NEXT", for: .normal)
+        
+        
+    }
+    
+    
 }
 
